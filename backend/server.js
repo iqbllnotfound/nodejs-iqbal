d2c3777816a886e67ffa617fe9b918a10234fff2
Iqbal <mr.zhaxt@gmail.com>
2024-09-04 08:41:08 +0700

    bla

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@
 const express = require ('express');
 const app = express();
 const port = 5000;
-
+const logRequest = require('./middleware/errorMiddleware')
 // testing APIs (Application Programming Interface)
 
 // app.use('/',(req,res) => {
@@ -34,3 +34,4 @@ app.listen(port, () => {
 })
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
+app.use(logRequest)
